4af0732e90d8b940b85ee61f0e55ec62585061ee
Szanyi Krisztián <szkript@gmail.com>
2019-12-13 15:40:29 +0100

    fixed infinite loop

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -11,6 +11,7 @@ namespace BFS_c_sharp
         {
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
+            Console.WriteLine($"users: {users.Count}");
             int res = MinDistance(users[0], users[2]);
             foreach (var user in users)
             {
@@ -29,19 +30,24 @@ namespace BFS_c_sharp
 
         public static int MinDistance(UserNode user, UserNode user2)
         {
+            int testCount = 0;
             Queue que = new Queue();
             que.Enqueue(user);
+            List<UserNode> visited = new List<UserNode>();
             while(que.Count > 0)
             {
                 UserNode actualVisit = (UserNode)que.Dequeue();
+                if (visited.Contains(actualVisit)) { continue; }
                 foreach(UserNode neighbor in actualVisit.Friends)
                 {
-                    if (!que.Contains(neighbor))
-                    {
-                        que.Enqueue(neighbor);
-                    }
+                    testCount++;
+                    que.Enqueue(neighbor);
+                    Console.WriteLine($"Runtime -> test count: {testCount}");
+
                 }
+                visited.Add(actualVisit);
             }
+            Console.WriteLine($"test count: {testCount}");
             return 0;
         }
     }
