1db475f013a16a871905f312bc7b1e3417afbd09
Szanyi Krisztián <szkript@gmail.com>
2019-12-13 11:28:58 +0100

    basic que implementation, lets teszt

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BFS_c_sharp.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace BFS_c_sharp
@@ -10,14 +11,38 @@ namespace BFS_c_sharp
         {
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
-
+            int res = MinDistance(users[0], users[2]);
             foreach (var user in users)
             {
-                Console.WriteLine(user);
+                //Console.WriteLine(user);
             }
 
+            Console.WriteLine(res);
             Console.WriteLine("Done");
             Console.ReadKey();
+        }
+
+        public void FriendsOfFriendsDistance(UserNode user)
+        {
+
+        }
+
+        public static int MinDistance(UserNode user, UserNode user2)
+        {
+            Queue que = new Queue();
+            que.Enqueue(user);
+            while(que.Count > 0)
+            {
+                UserNode actualVisit = (UserNode)que.Dequeue();
+                foreach(UserNode neighbor in actualVisit.Friends)
+                {
+                    if (!que.Contains(neighbor))
+                    {
+                        que.Enqueue(neighbor);
+                    }
+                }
+            }
+            return 0;
         }
     }
 }
