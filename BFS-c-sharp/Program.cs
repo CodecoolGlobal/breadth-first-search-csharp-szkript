2a47dcbd76070ad2a6d1699027e06099b6d1982a
Szanyi Krisztián <szkript@gmail.com>
2019-12-14 22:00:59 +0100

    blank

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -26,28 +26,7 @@ namespace BFS_c_sharp
         public static void FriendsOfFriendsDistance(UserNode user)
         {
             Console.WriteLine($"user info -> {user.FirstName} {user.LastName} (friends: {user.Friends.Count})");
-            Queue queue = new Queue();
-            List<int> depthDistance = new List<int>();
-            HashSet<int> userMap = new HashSet<int>();
-            for(int i=0; i <= user.Friends.Count; i++)
-            {
-                int depth = 0;
-                foreach(UserNode friend in user.Friends)
-                {
-                    Console.WriteLine($"|depth: {depth} -> {friend.FirstName} {friend.LastName} (friends: {friend.Friends.Count})");
-                    if(depth == 0)
-                    {
-                        queue.Enqueue(friend);
-                    }
-                    depth++;
-                    Console.WriteLine("->");
-                    foreach(UserNode friendOfFriend in friend.Friends)
-                    {
-                        Console.WriteLine($"-> |depth: {depth} -> {friendOfFriend.FirstName} {friendOfFriend.LastName} (friends: {friendOfFriend.Friends.Count})");
-                    }
-                }
-                depthDistance.Add(depth);
-            }
+            
         }
 
         public static int MinDistance(UserNode user, UserNode user2)
