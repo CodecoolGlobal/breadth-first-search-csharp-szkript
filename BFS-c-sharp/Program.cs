fb64ae7edc58541bbd177b43c98dd132b501f03c
Szanyi Krisztián <szkript@gmail.com>
2019-12-14 23:02:19 +0100

    save

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -12,14 +12,15 @@ namespace BFS_c_sharp
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
             Console.WriteLine($"users: {users.Count}");
-            int res = MinDistance(users[0], users[2]);
+            ExtractChilds(users[0]);
+            //int res = MinDistance(users[0], users[2]);
             foreach (var user in users)
             {
                 //Console.WriteLine(user);
             }
-            
 
-            Console.WriteLine(res);
+
+            //Console.WriteLine(res);
             Console.WriteLine("Done");
             Console.ReadKey();
         }
@@ -36,17 +37,17 @@ namespace BFS_c_sharp
             que.Enqueue(user);
             List<UserNode> visited = new List<UserNode>();
             HashSet<int> userMap = new HashSet<int>();
-            while(que.Count > 0)
+            while (que.Count > 0)
             {
                 UserNode actualVisit = (UserNode)que.Dequeue();
                 if (visited.Contains(actualVisit)) { continue; }
                 Console.WriteLine($"Runtime -> Actual : {actualVisit.FirstName} {actualVisit.LastName} (friends: {actualVisit.Friends.Count} )");
-                foreach(UserNode neighbor in actualVisit.Friends)
+                foreach (UserNode neighbor in actualVisit.Friends)
                 {
                     testCount++;
                     que.Enqueue(neighbor);
                     Console.WriteLine($"Runtime -> test count: {testCount}, {neighbor.FirstName} {neighbor.LastName} (friends: {neighbor.Friends.Count} )");
-                    if(neighbor == user2)
+                    if (neighbor == user2)
                     {
                         Console.WriteLine("MEGVAGY GECI");
                     }
@@ -62,6 +63,33 @@ namespace BFS_c_sharp
         public static void ShortestDistance()
         {
 
+        }
+        public static Queue ExtractChilds(UserNode user)
+        {
+            Queue result = new Queue();
+            LinkedList<UserNode> friendOrder = new LinkedList<UserNode>();
+            friendOrder.AddFirst(user);
+            int kamucounter = 0;
+            result.Enqueue(user);
+
+            while (result.Count > 0)
+            {
+                foreach (UserNode friend in user.Friends)
+                {
+                    if (kamucounter == 1)
+                    {
+                        kamucounter = 0;
+                        break;
+                    }
+                    friendOrder.AddLast(friend);
+                    result.Enqueue(friend);
+                    kamucounter++;
+                }
+
+            }
+            Console.WriteLine(friendOrder);
+
+            return result;
         }
     }
 }
